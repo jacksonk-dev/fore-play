4ea28a6bc92383d31fc0c90b518c185ea4b2fe5e
je-kay <jacksonk.dev@gmail.com>
2020-01-14 11:13:08 +0300

    Makes app light grey in color

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components'
+
 import ColorPanel from '../components/color-panel';
 
+const Wrapper = styled.div`
+  background-color: #F5F5F5;
+  height: 100vh;
+`
 function App() {
   return (
-    <div>
+    <Wrapper>
       <ColorPanel />
-    </div>
+    </Wrapper>
   );
 }
 
