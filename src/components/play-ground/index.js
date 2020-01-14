b21f6ef44b60edb6bc1f324d0ddcab273f064884
je-kay <jacksonk.dev@gmail.com>
2020-01-14 12:48:34 +0300

    Creates play ground

---
+++ src/components/play-ground/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+
+`;
+
+const PlayGround = () => (
+  <Wrapper>
+    Play Ground
+  </Wrapper>
+);
+
+export default PlayGround;
