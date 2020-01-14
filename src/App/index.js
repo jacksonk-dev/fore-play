61460d9d97015a32a03d5cf1735ff0f546da6610
je-kay <jacksonk.dev@gmail.com>
2020-01-14 12:50:10 +0300

    Displays play ground and side actions panel

--- src/App/index.js
+++ src/App/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 
 import ColorPanel from '../components/color-panel';
+import SideActionsPanel from '../components/side-actions-panel';
+import PlayGround from '../components/play-ground';
 
 const Wrapper = styled.div`
-  background-color: #F5F5F5;
   height: 100vh;
+  background-color: #F5F5F5;
+  display: flex;
+  flex-direction: column;
+`;
+
+const InnerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  flex-grow: 1;
 `;
 
 function App() {
   return (
     <Wrapper>
       <ColorPanel />
+      <InnerWrapper>
+        <PlayGround />
+        <SideActionsPanel />
+      </InnerWrapper>
     </Wrapper>
   );
 }
