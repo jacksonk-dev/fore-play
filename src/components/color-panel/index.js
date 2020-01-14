5b84763010dcd3ed0fb19cf4f410f7d14e13a4cf
je-kay <jacksonk.dev@gmail.com>
2020-01-14 11:12:54 +0300

    Improves styling of color panel

--- src/components/color-panel/index.js
+++ src/components/color-panel/index.js
@@ -3,15 +3,17 @@ import ColorPicker from '../../form-views/color-picker';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+    padding: .8rem;
+    border-radius: 0 0 0 1.5rem;
     background-color: #fff;
-    padding: 1rem;
+    box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
 `
 
 const ColorPanel = () => (
     <Wrapper>
         <ColorPicker 
-            width="500px"
-            circleSize={40}
+            width="450px"
+            circleSize={35}
         />
     </Wrapper>
 )
