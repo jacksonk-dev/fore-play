24a64f4bbd3fb67160251600c2309539feef8e26
je-kay <jacksonk.dev@gmail.com>
2020-01-16 15:53:39 +0300

    Sets up color picker to use a value and an onChange

--- src/form-views/color-picker/index.js
+++ src/form-views/color-picker/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CirclePicker } from 'react-color';
 
-const ColorPicker = ({ width, circleSize }) => (
+const ColorPicker = ({
+  width, circleSize, value, onChange,
+}) => (
   <CirclePicker
+    color={value}
+    onChange={(color) => {
+      onChange(color.hex);
+    }}
     width={width}
     circleSize={circleSize}
   />
@@ -12,9 +18,12 @@ const ColorPicker = ({ width, circleSize }) => (
 ColorPicker.propTypes = {
   width: PropTypes.string,
   circleSize: PropTypes.number,
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
 };
 
 ColorPicker.defaultProps = {
+  value: undefined,
   width: undefined,
   circleSize: undefined,
 };
