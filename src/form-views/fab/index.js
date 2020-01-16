63dd24eece21a398a43cd7483f3d101983a1988a
je-kay <jacksonk.dev@gmail.com>
2020-01-16 11:03:26 +0300

    Tweaks fab a bit more

--- src/form-views/fab/index.js
+++ src/form-views/fab/index.js
@@ -10,31 +10,34 @@ const Wrapper = styled.div`
 const FabLabel = styled.span`
   color: #fff;
   background-color: #000;
-  margin-right: .7rem;
-  padding: .4rem;
+  margin-right: 1rem;
+  padding: .8rem;
   border-radius: 40%;
 `;
 
 const FabWrapper = styled.div`
-    width: 1.5rem;
-    height: 1.5rem;
+    width: 4rem;
+    height: 4rem;
     padding: 1rem;
     border-radius: 50%;
     display: flex;
     justify-content: center;
     align-items: center;
-    color: #fff;
-    background-color: #004D40;
+    color: ${({ hollow, color }) => (hollow ? { primary: '#01579B', secondary: '#b71c1c' }[color] : '#fff')};
+    background-color: ${({ hollow, color }) => (hollow ? 'inherit' : { primary: '#01579B', secondary: '#b71c1c' }[color])};
+    border: 2px solid ${({ color }) => ({ primary: '#01579B', secondary: '#b71c1c' }[color])};
     box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
 `;
 
-const Fab = ({ children, label, showLabel }) => (
+const Fab = ({
+  children, label, showLabel, color, hollow,
+}) => (
   <Wrapper>
     {label && showLabel
     && (
       <FabLabel>{label}</FabLabel>
     )}
-    <FabWrapper>
+    <FabWrapper color={color} hollow={hollow}>
       {children}
     </FabWrapper>
   </Wrapper>
@@ -44,11 +47,15 @@ Fab.propTypes = {
   children: PropTypes.node.isRequired,
   label: PropTypes.string,
   showLabel: PropTypes.bool,
+  color: PropTypes.string,
+  hollow: PropTypes.bool,
 };
 
 Fab.defaultProps = {
   label: undefined,
   showLabel: false,
+  color: 'primary',
+  hollow: false,
 };
 
 export default Fab;
