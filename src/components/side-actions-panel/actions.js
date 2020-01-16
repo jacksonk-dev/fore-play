af8413d98644c6dcfc62fd5378768cf0b3dc788a
je-kay <jacksonk.dev@gmail.com>
2020-01-16 15:55:20 +0300

    Adds onClick props for add and remove fabs

--- src/components/side-actions-panel/actions.js
+++ src/components/side-actions-panel/actions.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import {
-  FaUndoAlt as UndoIcon,
-  FaRedoAlt as RedoIcon,
   FaTrash as ClearIcon,
   FaBomb as ExplodeIcon,
   FaSpaceShuttle as GravitateIcon,
 } from 'react-icons/fa';
 import { GiImplosion as ImplosionIcon } from 'react-icons/gi';
+import {
+  MdAdd as AddIcon,
+  MdRemove as RemoveIcon,
+} from 'react-icons/md';
 
 export default [
   {
-    label: 'Undo',
-    Icon: <UndoIcon />,
+    label: 'Add Boxes',
+    onClick: 'addBoxes',
+    Icon: <AddIcon style={{ fontSize: '2rem' }} />,
   },
   {
-    label: 'Redo',
-    Icon: <RedoIcon />,
+    label: 'Remove Boxes',
+    onClick: 'removeBoxes',
+    Icon: <RemoveIcon style={{ fontSize: '2rem' }} />,
   },
   {
     label: 'Clear',
